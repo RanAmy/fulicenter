ae50afcf9e27dfa5223105a79c4320a023c6a5d0
zhaoran <2680131355@qq.com>
2017-01-10 17:47:13 +0800

    添加闪屏界面以及闪屏界面跳转到主页面

---
+++ app/src/main/java/cn/ucai/fulicenter/controller/activity/SplashActivity.java
@@ -0,0 +1,32 @@
+package cn.ucai.fulicenter.controller.activity;
+
+import android.os.Handler;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.utils.MFGT;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //  延时2秒，然后跳转到MainActivity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                MFGT.startActivity(SplashActivity.this,MainActivity.class);
+                MFGT.finish(SplashActivity.this);
+            }
+        },2000);
+    }
+}
---
+++ app/src/main/java/cn/ucai/fulicenter/utils/MFGT.java
@@ -0,0 +1,23 @@
+package cn.ucai.fulicenter.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import cn.ucai.fulicenter.R;
+
+/**
+ * Created by Administrator on 2017/1/10 0010.
+ */
+
+public class MFGT {
+    //  点Back键，直接关闭Activity
+    public static void finish(Activity activity) {
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_left_in,R.anim.push_right_out);
+    }
+
+    public static void startActivity(Activity context, Class<?> cla) {
+        context.startActivity(new Intent(context, cla));
+        context.overridePendingTransition(R.anim.push_right_in, R.anim.push_left_out);
+    }
+}
