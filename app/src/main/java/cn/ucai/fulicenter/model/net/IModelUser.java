4ae3b0b5c84b49528ceea8e9762b1f8f7cb90169
zhaoran <2680131355@qq.com>
2017-01-18 14:44:13 +0800

    上传用户头像

--- app/src/main/java/cn/ucai/fulicenter/model/net/IModelUser.java
+++ app/src/main/java/cn/ucai/fulicenter/model/net/IModelUser.java
@@ -2,6 +2,8 @@ package cn.ucai.fulicenter.model.net;
 
 import android.content.Context;
 
+import java.io.File;
+
 import cn.ucai.fulicenter.model.utils.OkHttpUtils;
 
 /**
@@ -14,4 +16,6 @@ public interface IModelUser {
     void register(Context context, String username, String usernick, String password,OnCompleteListener<String> listener);
 
     void updateNick(Context context, String username, String usernick, OnCompleteListener<String> listener);
+
+    void uploadAvatar(Context context, String username, File file, OnCompleteListener<String> listener);
 }
