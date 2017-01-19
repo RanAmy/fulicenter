6d0812f2106ef4cdd55a4a2d476239e0fb5cde2c
zhaoran <2680131355@qq.com>
2017-01-19 13:04:40 +0800

    收藏宝贝列表的下拉刷新、上拉加载

--- app/src/main/java/cn/ucai/fulicenter/controller/activity/CollectsActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/activity/CollectsActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -14,23 +16,27 @@ import butterknife.ButterKnife;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.application.FuLiCenterApplication;
 import cn.ucai.fulicenter.application.I;
-import cn.ucai.fulicenter.controller.adapter.NewGoodsAdapter;
+import cn.ucai.fulicenter.controller.adapter.CollectAdapter;
 import cn.ucai.fulicenter.model.bean.CollectBean;
 import cn.ucai.fulicenter.model.bean.NewGoodsBean;
 import cn.ucai.fulicenter.model.bean.User;
 import cn.ucai.fulicenter.model.net.DisplayUtils;
 import cn.ucai.fulicenter.model.net.IModelUser;
 import cn.ucai.fulicenter.model.net.OnCompleteListener;
+import cn.ucai.fulicenter.model.utils.CommonUtils;
 import cn.ucai.fulicenter.model.utils.ConvertUtils;
 import cn.ucai.fulicenter.model.utils.L;
 import cn.ucai.fulicenter.model.utils.SpaceItemDecoration;
+
+import static cn.ucai.fulicenter.application.I.ACTION_PULL_DOWN;
+import static cn.ucai.fulicenter.application.I.ACTION_PULL_UP;
 
 public class CollectsActivity extends AppCompatActivity {
     private static final String TAG = CollectsActivity.class.getSimpleName();
     User user;
     IModelUser model;
     GridLayoutManager gm;
-    NewGoodsAdapter mAdapter;
+    CollectAdapter mAdapter;
     int pageId = 1;
 
     @BindView(R.id.tvRefresh)
@@ -51,25 +57,83 @@ public class CollectsActivity extends AppCompatActivity {
             finish();
         } else {  //  若用户不为空，则实例化数据
             initView();
-            initData();
+            initData(I.ACTION_DOWNLOAD);
+            setListener();
         }
     }
 
-    private void initData() {
+    private void setListener() {
+        setPullUpListener();
+        setPullDownListener();
+    }
+
+    private void setPullDownListener() {
+        srl.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                srl.setRefreshing(true);
+                tvRefresh.setVisibility(View.INVISIBLE);
+                pageId = 1;
+//                downloadNewGoodsList(ACTION_PULL_DOWN,pageId);
+                initData(I.ACTION_PULL_DOWN);
+            }
+        });
+    }
+
+    private void setPullUpListener() {
+        rv.setOnScrollListener(new RecyclerView.OnScrollListener() {
+            int lastPosition ;
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+
+                if (newState == RecyclerView.SCROLL_STATE_IDLE
+                        && mAdapter.isMore()
+                        && lastPosition == mAdapter.getItemCount() - 1) {
+                    pageId ++;
+//                    downloadNewGoodsList(ACTION_PULL_UP,pageId);
+                    initData(I.ACTION_PULL_UP);
+                }
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                lastPosition= gm.findLastVisibleItemPosition();
+            }
+        });
+    }
+
+
+    private void initData(final int action) {
         model.getCollects(this, user.getMuserName(),pageId, I.PAGE_SIZE_DEFAULT, new OnCompleteListener<CollectBean[]>() {
             @Override
             public void onSuccess(CollectBean[] result) {
-                if (result == null) {
-
-                } else {
+                srl.setRefreshing(false);
+                tvRefresh.setVisibility(View.GONE);
+                mAdapter.setMore(true);
+                if (result != null && result.length > 0) {
                     ArrayList<CollectBean> list = ConvertUtils.array2List(result);
-                    L.e(TAG, "list=" + list.size());
+                    if (action == I.ACTION_DOWNLOAD || action == I.ACTION_PULL_DOWN) {
+                        mAdapter.initData(list);
+                    } else {
+                        mAdapter.addData(list);
+                    }
+                    if (list.size() < I.PAGE_SIZE_DEFAULT) {
+                        mAdapter.setMore(false);
+                    }
+                } else {
+                    mAdapter.setMore(false);
                 }
             }
 
             @Override
             public void onError(String error) {
-
+                srl.setRefreshing(false);
+                tvRefresh.setVisibility(View.GONE);
+                mAdapter.setMore(false);
+                CommonUtils.showShortToast(error);
+                L.e(TAG,"error="+error);
             }
         });
     }
@@ -85,7 +149,7 @@ public class CollectsActivity extends AppCompatActivity {
         rv.addItemDecoration(new SpaceItemDecoration(12));
         rv.setLayoutManager(gm);
         rv.setHasFixedSize(true);
-        mAdapter = new NewGoodsAdapter(this, new ArrayList<NewGoodsBean>());
+        mAdapter = new CollectAdapter(this, new ArrayList<CollectBean>());
         rv.setAdapter(mAdapter);
     }
 }
