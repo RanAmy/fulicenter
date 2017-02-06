953ef5f581ffedecd461774896d47d603ef19e56
zhaoran <2680131355@qq.com>
2017-02-06 14:16:49 +0800

    添加订单功能

--- app/src/main/java/cn/ucai/fulicenter/controller/activity/OrderActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/controller/activity/OrderActivity.java
@@ -2,14 +2,18 @@ package cn.ucai.fulicenter.controller.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.fulicenter.R;
 import cn.ucai.fulicenter.application.I;
+import cn.ucai.fulicenter.model.net.DisplayUtils;
 
 public class OrderActivity extends AppCompatActivity {
     int payPrice = 0;
@@ -30,10 +34,47 @@ public class OrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_order);
         ButterKnife.bind(this);
         payPrice = getIntent().getIntExtra(I.Cart.PAY_PRICE, 0);
+        DisplayUtils.initBackWithTitle(this, "填写收货地址");
         setView();
     }
 
     private void setView() {
-        tvOrderPrice.setText("合计：￥"+payPrice);
+        tvOrderPrice.setText("合计：￥" + payPrice);
+    }
+
+    @OnClick(R.id.tv_order_buy)
+    public void checkOrder() {
+        String receiveName = edOrderName.getText().toString();
+        if (TextUtils.isEmpty(receiveName)) {
+            edOrderName.setError("收货人姓名不能为空");
+            edOrderName.requestFocus();
+            return;
+        }
+
+        String mobile = edOrderPhone.getText().toString();
+        if (TextUtils.isEmpty(mobile)) {
+            edOrderPhone.setError("手机号不能为空");
+            edOrderPhone.requestFocus();
+            return;
+        }
+        if (!mobile.matches("[\\d]{11}")) {
+            edOrderPhone.setError("手机号码格式错误");
+            edOrderPhone.requestFocus();
+            return;
+        }
+
+        String area = spinOrderPhone.getSelectedItem().toString();
+        if (TextUtils.isEmpty(area)) {
+            Toast.makeText(OrderActivity.this, "收货地区不能为空", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        String address = edOrderStreet.getText().toString();
+        if (TextUtils.isEmpty(address)) {
+            edOrderStreet.setError("街道地址不能为空");
+            edOrderStreet.requestFocus();
+            return;
+        }
     }
 }
+
